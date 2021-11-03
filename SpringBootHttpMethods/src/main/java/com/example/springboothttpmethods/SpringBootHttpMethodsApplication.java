02074ff9bdd26b6ca4081f3fe0f82d2d1059873d
EST <ghostrekon5500@ya.ru>
2021-11-03 14:51:55 +0300

    init

---
+++ SpringBootHttpMethods/src/main/java/com/example/springboothttpmethods/SpringBootHttpMethodsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothttpmethods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpMethodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHttpMethodsApplication.class, args);
+    }
+
+}
