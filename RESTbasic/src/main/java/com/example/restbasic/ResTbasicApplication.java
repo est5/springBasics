d0752f365c9736e08025ecfcb73f596544031db9
EST <ghostrekon5500@ya.ru>
2021-11-04 15:09:36 +0300

    init

---
+++ RESTbasic/src/main/java/com/example/restbasic/ResTbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.restbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTbasicApplication.class, args);
+    }
+
+}
---
+++ RESTbasic/src/test/java/com/example/restbasic/ResTbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTbasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
