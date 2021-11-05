06302cdb5619f97fe5d19b09b1b61cf0860c4530
EST <ghostrekon5500@ya.ru>
2021-11-05 20:10:15 +0300

    init

---
+++ RESTconsuming/src/main/java/com/example/restconsuming/ResTconsumingApplication.java
@@ -0,0 +1,13 @@
+package com.example.restconsuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTconsumingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTconsumingApplication.class, args);
+    }
+
+}
---
+++ RESTconsuming/src/test/java/com/example/restconsuming/ResTconsumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restconsuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTconsumingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
