b9af1a09e6b30632c34a320e5c1e5919c85a67e0
EST <ghostrekon5500@ya.ru>
2021-11-06 14:00:22 +0300

    rest templates

---
+++ RESTconsuming/RESTTemplate/src/main/java/com/example/resttemplate/controller/PaymentsController.java
@@ -0,0 +1,26 @@
+package com.example.resttemplate.controller;
+
+import com.example.resttemplate.model.Payment;
+import com.example.resttemplate.proxy.PaymentsProxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentsController {
+
+    private final PaymentsProxy paymentsProxy;
+
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(
+            @RequestBody Payment payment
+    ) {
+        return paymentsProxy.createPayment(payment);
+    }
+}
