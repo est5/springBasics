dcc6330f8970c176b8d6871c5dd5e0a67cf08414
EST <ghostrekon5500@ya.ru>
2021-11-06 17:40:54 +0300

    jdbc

---
+++ dataSources/src/main/java/com/example/datasources/DataSourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.datasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSourcesApplication.class, args);
+    }
+
+}
---
+++ dataSources/src/main/java/com/example/datasources/repository/PurchaseRepository.java
@@ -0,0 +1,38 @@
+package com.example.datasources.repository;
+
+import com.example.datasources.model.Purchase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc; // from springboot pom
+
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void storePurchase(Purchase purchase){
+        String sql =
+                "INSERT INTO purchase VALUES(NULL, ?, ?)";
+        jdbc.update(sql,
+                purchase.getProduct(),
+                purchase.getPrice());
+    }
+
+    public List<Purchase> findALlPurchases(){
+        String sql = "SELECT * FROM purchase";
+
+        RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
+            Purchase rowObject = new Purchase();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setProduct(r.getString("product"));
+            rowObject.setPrice(r.getBigDecimal("price"));
+            return rowObject;
+        };
+        return jdbc.query(sql, purchaseRowMapper);
+    }
+}
